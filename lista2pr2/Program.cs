58a1a4d9ff171afd550feb4ce59b78a9f29bc4c4
gabrelovardex <pinto.g@aluno.ifsp.edu.br>
2023-03-22 11:12:23 -0300

    Adicionar arquivos de projeto.

---
+++ lista2pr2/Program.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // exercicio1
+
+            //double laranjas, preco1;
+
+            //Console.WriteLine("Quantas laranjas você comprou?");
+            //laranjas = double.Parse(Console.ReadLine());
+
+            //if (laranjas < 12)
+            //{
+            //    preco1 = laranjas * 0.35;
+            //    Console.WriteLine("O valor de laranjas é: " + preco1);
+
+            //}
+            //else
+            //{
+
+
+            //    preco1 = laranjas * 0.28;
+
+            //    Console.WriteLine("O valor de laranjas é:" + preco1);
+            //}
+
+            //Console.ReadKey();
+
+            //--------------------------------------------------------------------------------------------------------------------
+
+            //exercicio2
+
+            //int ano;
+
+            //Console.WriteLine("Me conte,em que ano você nasceu");
+            //ano = int.Parse(Console.ReadLine());
+
+            //if (ano <= 2007)
+            //    Console.WriteLine("Você pode votar");
+
+            //else
+            //    Console.WriteLine("Você não pode votar");
+            //    Console.ReadKey();
+
+            //--------------------------------------------------------------------------------------------------------------------
+
+            //exercicio3
+
+            //float lado1, lado2, lado3;
+
+            //Console.WriteLine("Quanto mede o primeiro lado do seu Triangulo? ");
+            //lado1 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Quanto mede o segundo lado do seu Triangulo? ");
+            //lado2 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Quanto mede o terceiro lado do seu Triangulo? ");
+            //lado3 = float.Parse(Console.ReadLine());
+
+            //if (lado1 == lado2 && lado2 == lado3 && lado3 == lado1)
+            //    Console.WriteLine("O seu Triangulo é Equilatero");
+
+            //else if (lado1 != lado2 && lado2 != lado3 && lado3 != lado1)
+            //    Console.WriteLine("O seu Triangulo é Escaleno");
+
+            //else
+            //    Console.WriteLine("O seu Triangulo é Isoceles");
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------------------------------------------------------------
+
+            //exercicio4
+
+            //double delta, a, b, c, raiz1, raiz2;
+
+            //Console.WriteLine("qual o valor de a");
+            //a = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("qual o valor de b");
+            //b = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("qual o valor de c");
+            //c = double.Parse(Console.ReadLine());
+
+            //delta = (b * b) - 4 * a * c;
+            //raiz1 = ((-b + (Math.Sqrt(delta))) / (2 * a));
+            //raiz2 = ((-b - (Math.Sqrt(delta))) / (2 * a));
+
+
+            //Console.WriteLine("a raiz 1 é " + raiz1);
+            //Console.WriteLine("a raiz 2 é " + raiz2);
+
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------------------------------------------------------------
+
+            //exercicio5
+
+            //int numero;
+
+            //Console.Write("digite un numero: ");
+            //numero = int.Parse(Console.ReadLine());
+
+            //if (numero % 3 == 0 && numero % 5 == 0)
+            //{
+            //    Console.WriteLine("o numero é divisil por 3 e 5");
+            //}
+            //else
+            //{
+
+            //    if (numero % 3 == 0)
+            //    { Console.WriteLine("o numero è divisivel por 3"); }
+
+            //    else
+            //    {
+            //        if (numero % 5 == 0)
+            //        {
+            //            Console.WriteLine(" o numero é divisivel por 5");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("o numero nao é divisivel por 3 nem por 51");
+
+
+            //        }
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+            //----------------------------------------------------------------------------------------------------------------------
+
+            // exercicio6
+
+            //double A1, A2, A3;
+
+            //Console.WriteLine("Digite o primeiro angulo");
+            //A1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o segundo angulo");
+            //A2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o terceiro angulo");
+            //A3 = double.Parse(Console.ReadLine());
+
+            //if ((A1 == 90) || (A2 == 90) || (A3 == 90))
+            //     Console.WriteLine("Seu triângulo é um triângulo retângulo ");
+
+            // else if ((A1 > 90) || (A2 > 90) || (A3 > 90))
+            //    Console.WriteLine("Seu triângulo é um triângulo obtusângulo ");
+
+            //else
+            //    Console.WriteLine("Seu triângulo é um triângulo de três ângulos agudos");
+
+
+            //Console.ReadKey();
+
+            //---------------------------------------------------------------------------------------------------------------------
+
+            //exercicio7
+
+            // int A, B, C;
+
+            // Console.WriteLine("qual o valor do numero ");
+            // A = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("qual o valor do numero ");
+            // B = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("qual o valor do numero ");
+            // C = int.Parse(Console.ReadLine());
+
+            // if (A < B && B < C)
+            //   Console.WriteLine(A +" "+ B+" " + C);
+
+            // else if (A < B && B > C)
+            // Console.WriteLine(a + " " + c + " "+b);
+
+            // else if (B < A && A < C)
+            // Console.WriteLine(B + " " + A + " "+C);
+
+            //   else if (B < A && A > C)
+            // Console.WriteLine(B + " " + C + " "+A);
+
+            // else if (C < A && A < B)
+            // Console.WriteLine(C + " " + A + " "+B);
+
+            // else if (C < A && A > C)
+            // Console.WriteLine(C + " " + B + " "+A);
+
+            // Console.ReadKey();
+
+            //-----------------------------------------------------------------------------------------------------------------------
+
+            //exercicio8
+
+            // int ANO;
+
+            // Console.Write("Digite um ano: ");
+            // ANO = int.Parse(Console.ReadLine());
+
+
+            // if ((ANO % 4 == 0 && ANO % 100 != 0) || (ANO % 400 == 0)){
+            //
+            //     Console.WriteLine(ANO + " é um ano bissexto");
+
+            // }else{
+
+            //     Console.WriteLine(ANO + " não é um ano bissexto");
+
+            // }
+
+            // Console.ReadKey();
+
+            //-----------------------------------------------------------------------------------------------------------------------
+
+            //exercicio10
+
+            //Na linguagem C#, existem várias formas de gerar números aleatórios, tanto inteiros como reais. A seguir, apresento duas maneiras comuns de fazer isso:
+
+//            Classe Random
+//A classe Random é uma das formas mais comuns de gerar números aleatórios em C#. Ela é parte da biblioteca padrão da linguagem e permite gerar números inteiros e reais de forma aleatória.
+
+
+
+
+      }
+
+    }
+
+}
